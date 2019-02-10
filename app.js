6a07576eac0dc2a1c4e5fca50c6e3eb368dbaabb
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-02-10 00:25:21 -0800

    Integrated socket.io

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const exphbs = require('express-handlebars');
 const app = express();
 const server = require('http').Server(app);
 
+// Socket.io
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log('New user connected!');
+})
+
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
 
 
 // HTTP: Index
